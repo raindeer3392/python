25d49834a519bc1ba2166a7a05f44b53afcd85cc
raindeer3392 <raindeer3392@naver.com>
2024-03-25 11:42:46 +0900

    11:30

---
+++ Bool_test.py
@@ -0,0 +1,15 @@
+def main():
+
+    for q in range(1,6,1):
+        for w in range (1,10,1):
+            print(q*w, end=", ")
+        print("\n")
+        continue
+    
+    for e in range(7,10,1):
+        for r in range(1,10,1):
+            print(e*r, end=", ")
+        print("\n")
+
+
+main()
